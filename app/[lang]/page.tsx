3a16fd53646b21be037c65489d8f0ac5dbcb8255
GoodPointt <dev6012@gmail.com>
2024-01-11 17:49:56 +0200

    feat: Add ContactForm localization

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,15 +1,24 @@
+import { getDictionary } from '../lib/dictionary'
+import { IParams } from '../lib/interfaces'
 import Contact from '../ui/contact/contact/Contact'
 import Represent from '../ui/contact/represent/Represent'
 import Hero from '../ui/landing/hero/Hero'
 import Projects from '../ui/work/Projects'
 
-const LandingPage = () => {
+const LandingPage: React.FC<IParams> = async ({ params: { lang } }) => {
+	const {
+		// hero,
+		// projects,
+		// represent,
+		contact,
+	} = await getDictionary(lang)
+
 	return (
 		<>
 			<Hero />
 			<Projects />
 			<Represent />
-			<Contact />
+			<Contact contacts={contact.contacts} contactForm={contact.contactForm} />
 		</>
 	)
 }
